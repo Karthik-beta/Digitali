6a11eeb1f4ca0ad5ec1ba7310725a73da6c27db4
Karthik-beta <12345678+Karthik-beta@users.noreply.github.com>
2024-08-13 18:29:38 +0530

    1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -33,8 +33,8 @@ from celery import Celery
 from celery.schedules import crontab, timedelta
 
 # Celery settings
-CELERY_BROKER_URL = 'amqp://guest:guest@localhost:5672'
-CELERY_RESULT_BACKEND = 'rpc://'
+CELERY_BROKER_URL = 'redis://redis:6379/0'
+CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
 CELERY_ACCEPT_CONTENT = ['json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
