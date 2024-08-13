59fa95655840f6d489792a99db15cbad3e9a1b39
Karthik-beta <12345678+Karthik-beta@users.noreply.github.com>
2024-08-13 18:01:58 +0530

    1

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -110,8 +110,8 @@ DATABASES = {
         'NAME': 'skf',
         'USER': 'postgres',
         'PASSWORD': 'password123',
-        'HOST': '10.117.8.143',
-        # 'HOST': '127.0.0.1',
+        # 'HOST': '10.117.8.143',
+        'HOST': '172.23.16.1',
         'PORT': '5433',
     }
     # 'default': {
