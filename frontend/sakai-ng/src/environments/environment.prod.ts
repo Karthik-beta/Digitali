f11748602be9d5d69630c44c08bcfb2cde4389e4
Karthik-beta <karthikpar@protonmail.com>
2024-10-23 16:52:19 +0530

    1

--- frontend/sakai-ng/src/environments/environment.prod.ts
+++ frontend/sakai-ng/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-//   APIUrl: 'http://192.168.0.108:8000'
-  APIUrl: process.env['API_URL'] || 'http://localhost:8000'
+  APIUrl: 'http://192.168.0.108:8000'
+//   APIUrl: process.env['API_URL'] || 'http://localhost:8000'
 };
