5e63aea9b96eb935bc771e763f56a694a379dd0d
Karthik-beta <karthikpar@protonmail.com>
2024-10-24 14:21:34 +0530

    Update environment.prod.ts

--- frontend/sakai-ng/src/environments/environment.prod.ts
+++ frontend/sakai-ng/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
 //   APIUrl: 'http://192.168.0.108:8000'
-    APIUrl: 'http://django:8000'
+    APIUrl: 'http://localhost:8000'
 //   APIUrl: process.env['API_URL'] || 'http://localhost:8000'
 };
